277a9f25d3d456ae4bd8e6e26e23aefaed9c913a
Jose Zuluaga <josezuluaga97@gmail.com>
2022-12-19 18:35:13 -0500

    primeros cambios app

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_app/api/api.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: Text("Home Screen"),),
+      body: Column(children: [
+        
+      ]),
     );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    homeRequest().then((value) => {
+      
+    });
   }
 }
